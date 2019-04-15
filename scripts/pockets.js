6899fec4157d6d63615cb6c4da78cfc6b975f774
Moki <love0.0chen@gmail.com>
2019-04-15 21:27:17 +0800

    修改推荐文章的渲染方式

--- scripts/pockets.js
+++ scripts/pockets.js
@@ -3,35 +3,75 @@ var pockets = [];
 var store = localforage.createInstance({
   name: "webhulu.com"
 });
+window.AV.init('LK2HVhuVJrNVf0yJnST2TVNb-gzGzoHsz', 'KNyncdaxOVjveO3PmJXmBthl');
+var Post = window.AV.Object.extend('articles');
+
 var vm = new Vue({
   el: '#pockets',
   data: {
-    offset_end: 11,
+    total: 0,
+    loading: false,
+    offset_end: 0,
     db: pockets
   },
   methods: {
+    getTotal: function(){
+      const me = this;
+      return Post.query.count().then(function (count) {
+        me.total = count;
+        console.log(me.total);
+      });
+    },
+    loadData: function(){
+      const me = this;
+      this.loading = true;
+      return Post.query
+        .skip(me.offset_end)
+        .limit(10)
+        .exists('excerpt')
+        .exists('given_title')
+        .notEqualTo('excerpt', '')
+        .notEqualTo('given_title', '')
+        .find()
+        .then(function (response) {
+          const newArticles = response.map(function (record) {
+            var ret = record.toJSON();
+            return ret;
+          });
+          me.db = (me.db.concat(newArticles)).slice();
+          me.loading = false;
+        });
+    },
     loadMore: function(){
+      if(this.loading){ return false;}
       let offset_end = this.offset_end;
       offset_end +=  12;
       this.offset_end = offset_end;
+      this.loadData();
     }
+  },
+  mounted: function () {
+    const me = this;
+    me.$nextTick(function () {
+      me.getTotal().then(function(){
+        me.loadData();
+      });
+    })
   },
   computed: {
     pockets: function(){
-      return this.db.slice(0, this.offset_end);
+      return this.db;
     }
   }
 });
-window.onload = function() {
-  getItems();
-  var myRequest = new Request('/pockets.json');
-  fetch(myRequest).then(function(response) {
-    return response.json();
-  }).then(function(response) {
-    let i=0, cnt = response.length;
-    save(0, cnt-1, response);
-  });
-}
+
+$(function () {
+  $(window).on('scroll', function () {
+    if($('#pocket-load-more:in-viewport').length){
+      vm.loadMore();
+    };
+  })
+});
 
 function save(offset, maxOffset, list){
   if( offset < maxOffset){
@@ -52,6 +92,7 @@ function save(offset, maxOffset, list){
 function getItems(){
   var items = [];
   store.iterate(function(value, key, iterationNumber) {
+    console.error(value);
     if( value && value.url && value.excerpt ){
       items.push(value);
     }
